ceb788e1833dd33230da11f27046b5c13e2411d8
Kevin Funk <kevin@kevinfunk.com>
2021-04-19 12:50:42 -0400

    Refactors the download controller.

--- src/Controller/DownloadController.php
+++ src/Controller/DownloadController.php
@@ -2,48 +2,20 @@
 
 namespace Drupal\default_content_ui\Controller;
 
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\system\FileDownloadController;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * Returns responses for config module routes.
+ * DownloadController.
  */
-class DownloadController implements ContainerInjectionInterface {
-
-  /**
-   * The file download controller.
-   *
-   * @var \Drupal\system\FileDownloadController
-   */
-  protected $fileDownloadController;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      new FileDownloadController()
-    );
-  }
-
-  /**
-   * Constructs a ConfigController object.
-   *
-   * @param \Drupal\system\FileDownloadController $file_download_controller
-   *   The file download controller.
-   */
-  public function __construct(FileDownloadController $file_download_controller) {
-    $this->fileDownloadController = $file_download_controller;
-  }
+class DownloadController extends FileDownloadController {
 
   /**
    * Downloads a tarball of export.
    */
   public function downloadExport() {
     $request = new Request(['file' => 'default_content.tar.gz']);
-    return $this->fileDownloadController->download($request, 'temporary');
+    return $this->download($request, 'temporary');
   }
 
 }
