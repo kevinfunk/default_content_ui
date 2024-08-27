dc1d01689d9b58997f915eebf8447e721eb57e83
Kevin Funk <kevin@kevinfunk.com>
2024-08-27 09:22:15 -0400

    Updates to use standard default content fuctions.

--- src/Form/ExportForm.php
+++ src/Form/ExportForm.php
@@ -249,10 +249,10 @@ class ExportForm extends FormBase {
     $entities = \Drupal::entityQuery($entity_type)->accessCheck(TRUE)->execute();
     foreach ($entities as $entity_id) {
       if ($mode === 'references') {
-        $exporter->exportContent($entity_type, $entity_id, $references = true, $folder);
+        $exporter->exportContentWithReferences($entity_type, $entity_id, $folder);
       }
       else {
-        $exporter->exportContent($entity_type, $entity_id, $references = false, $folder);
+        $exporter->exportContent($entity_type, $entity_id, $folder);
       }
 
     }
